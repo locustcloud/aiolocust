47333019086df79183e8bf810d502c5b90498c85
ftb-skry <fredrik.berts@skry.se>
2025-01-08 18:09:24 +0100

    Create locustfile.py

---
+++ locustfile.py
@@ -0,0 +1,22 @@
+from locust import User, events
+
+
+@events.init.add_listener
+def on_init(environment, **kwargs):
+    # Set up the event loop
+
+    import asyncio
+    import gevent.selectors
+
+    class EventLoop(asyncio.SelectorEventLoop):
+        def __init__(self, environment):
+            self.__environment = environment
+            super().__init__(gevent.selectors.DefaultSelector())
+
+        def run_forever(self):
+            greenlet = self.__environment.runner.greenlet.spawn(super(EventLoop, self).run_forever)
+            greenlet.join()
+
+    loop = EventLoop(environment)
+    environment.loop = loop
+    asyncio.set_event_loop(loop)
